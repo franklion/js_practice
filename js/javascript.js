1f095bb9dd78688f8a8805357ea48b4efa5b1f4d
frankchen <frankchen@fashionguide.com.tw>
2015-01-03 13:32:19 +0800

    新增getPara function

--- js/javascript.js
+++ js/javascript.js
@@ -1,10 +1,10 @@
 window.onload = function() {
   
   var data = { 
-      'name': '小明',
-      'phone': '0987654321',  
-      'website': 'http://www.fashionguide.com.tw',
-      'img': 'https://piposay.s3.amazonaws.com/pipo/44b226c7-5883-4bc7-9df4-18636eddb23a.png'
+    'name': '小明',
+    'phone': '0987654321',  
+    'website': 'http://www.fashionguide.com.tw',
+    'img': 'https://piposay.s3.amazonaws.com/pipo/44b226c7-5883-4bc7-9df4-18636eddb23a.png'
   };
 
   /* 取出id為template所包覆的所有元素 return string */ 
@@ -17,14 +17,29 @@ window.onload = function() {
 
   /* template 替換 */
   function renderer(data,html){
-  /* 取出JSON中key */
-  var key = Object.keys(data);
+    /* 取出JSON中key */
+    var key = Object.keys(data);
 
-  for(var i = 0 ; i < key.length ; i++){
+    for(var i = 0 ; i < key.length ; i++){
       var temp = "{" + key[i] + "}";
       var reg = new RegExp(temp,"g");
       html = html.replace( reg ,data[key[i]]);
-  }
-     return html;
+    }
+      return html;
   };
+
+  /* call getPara function 取得網址當中指定名稱的參數值 */
+  var value = getPara("id");
+
+  function getPara(key){
+    var query = window.location.search.substring(1);
+    var array = query.split("&");
+    for(var i = 0 ; i < array.length ; i++){
+      var sub_array = array[i].split("=");
+      if(sub_array[0] == key){return sub_array[1]};
+    }
+    return false;
+  }
+
+  console.log(value);
 }
