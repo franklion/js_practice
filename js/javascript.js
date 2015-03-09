54caac0e6de50ebb6ecb23d747ba4f53aa4f3259
frankchen <frankchen@fashionguide.com.tw>
2015-03-09 11:12:29 +0800

    第二版 使用script 包覆template區塊

--- js/javascript.js
+++ js/javascript.js
@@ -1,31 +1,30 @@
 window.onload = function() {
   
-  /* JSON */
   var data = { 
- 	'name': '小明',
-	'phone': '0987654321',	
-	'website': 'http://www.fashionguide.com.tw',
-	'img': 'https://piposay.s3.amazonaws.com/pipo/44b226c7-5883-4bc7-9df4-18636eddb23a.png'
+      'name': '小明',
+      'phone': '0987654321',  
+      'website': 'http://www.fashionguide.com.tw',
+      'img': 'https://piposay.s3.amazonaws.com/pipo/44b226c7-5883-4bc7-9df4-18636eddb23a.png'
   };
 
-  /* 取出id為main所包覆的所有元素 return string */
-  var main = document.getElementById("main").innerHTML;  
+  /* 取出id為template所包覆的所有元素 return string */ 
+  var template = document.getElementById("template").innerHTML;
   
   /* call renderer function 替換特定字串*/
-  html = renderer(data,main);
+  html = renderer(data, template);
   /* 更新template */
-  document.getElementById("main").innerHTML = html
+  document.getElementById("main").innerHTML = html;
 
   /* template 替換 */
   function renderer(data,html){
-    /* 取出JSON中key */
-    var key = Object.keys(data);
+  /* 取出JSON中key */
+  var key = Object.keys(data);
 
-    for(var i = 0 ; i < key.length ; i++){
+  for(var i = 0 ; i < key.length ; i++){
       var temp = "{" + key[i] + "}";
       var reg = new RegExp(temp,"g");
-      html = html.replace( reg ,data[key[i]])
-    }
-    return html;
+      html = html.replace( reg ,data[key[i]]);
+  }
+     return html;
   };
 }
